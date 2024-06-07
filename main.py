9f98940842a31f8f37ea0c30dd44676f410253f3
Martin Härtel <Martin.Haertel@check24.de>
2024-06-07 19:59:40 +0200

    iuzew<thui

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("test")
+import os
+import discord
+from dotenv import load_dotenv
+from discord.commands import Option
+
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = discord.Bot(
+    intents=intents,
+    debug_guilds=[os.getenv('DEBUG_GUILD_ID')],
+)
+
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+
+
+bot.run(os.getenv('DISCORD_BOT_TOKEN'))
